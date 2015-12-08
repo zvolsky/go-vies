6d99083528f6247a55639d84d9bc7d0b1b9cc988
zvolsky <zvolsky@seznam.cz>
2015-12-08 10:35:22 +0100

    PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'go-vies',
+  packages = ['go-vies'],
+  version = '1.0',
+  description = 'VIES VAT Validation (like django-vies without Django dependencies)',
+  author = 'Mirek Zvolsky',
+  author_email = 'zvolsky@seznam.cz',
+  url = 'https://github.com/zvolsky/go-vies',
+  download_url = 'https://github.com/zvolsky/go-vies/tarball/1.0',
+  keywords = ['vies', 'vat'],
+  classifiers = [],
+)
