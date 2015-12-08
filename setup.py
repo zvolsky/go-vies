1d907bacbb11233a9eb0693a21a477288617ab25
zvolsky <zvolsky@seznam.cz>
2015-12-08 12:14:07 +0100

    F setup.py: go- -> go_

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'go-vies',
-  py_modules = ['go-vies'],
-  version = '1.0.1',
+  py_modules = ['go_vies'],
+  version = '1.0.2',
   description = 'VIES VAT Validation (like django-vies without Django dependencies)',
   author = 'Mirek Zvolsky',
   author_email = 'zvolsky@seznam.cz',
   url = 'https://github.com/zvolsky/go-vies',
-  download_url = 'https://github.com/zvolsky/go-vies/tarball/1.0.1',
+  download_url = 'https://github.com/zvolsky/go-vies/tarball/1.0.2',
   keywords = ['vies', 'vat'],
   classifiers = [],
 )
