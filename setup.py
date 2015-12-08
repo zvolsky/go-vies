98d7f99022821f54fb67e2fa4231e3e6fabcb9d7
zvolsky <zvolsky@seznam.cz>
2015-12-08 11:59:11 +0100

    changed version no of repaired version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'go-vies',
   py_modules = ['go-vies'],
-  version = '1.0',
+  version = '1.0.1',
   description = 'VIES VAT Validation (like django-vies without Django dependencies)',
   author = 'Mirek Zvolsky',
   author_email = 'zvolsky@seznam.cz',
   url = 'https://github.com/zvolsky/go-vies',
-  download_url = 'https://github.com/zvolsky/go-vies/tarball/1.0',
+  download_url = 'https://github.com/zvolsky/go-vies/tarball/1.0.1',
   keywords = ['vies', 'vat'],
   classifiers = [],
 )
