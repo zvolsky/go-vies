80cf48b37598dd0af4b4e41ea837d49a71e6e04c
zvolsky <zvolsky@seznam.cz>
2015-12-08 13:50:50 +0100

    added proper requires= into setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
   name = 'go-vies',
   py_modules = ['go_vies'],
-  version = '1.0.2',
+  version = '1.0.3',
   description = 'VIES VAT Validation (like django-vies without Django dependencies)',
+  requires = ['retrying (>=1.3.3)', 'suds (>=0.4)'],
   author = 'Mirek Zvolsky',
   author_email = 'zvolsky@seznam.cz',
   url = 'https://github.com/zvolsky/go-vies',
-  download_url = 'https://github.com/zvolsky/go-vies/tarball/1.0.2',
+  download_url = 'https://github.com/zvolsky/go-vies/tarball/1.0.3',
   keywords = ['vies', 'vat'],
   classifiers = [],
 )
